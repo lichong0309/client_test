c729ea17254d8cb312d2cf88637b4d33be2fc177
lichong0309 <chongli0309@gmail.com>
2021-06-14 14:37:55 +0900

    update

--- inter-operator-scheduler/examples/sample.py
+++ inter-operator-scheduler/examples/sample.py
@@ -30,7 +30,7 @@ def server():
 
 def  receiveInput():
     
-    channel = grpc.insecure_channel('163.143.0.101:56789')                  # 连上服务器
+    channel = grpc.insecure_channel('localhost:56789')                  # 连上服务器
     # with grpc.secure_channel('163.143.0.101:56789', grpc.ssl_channel_credentials()) as channel:
     print("connect the server...")
     stub = outputTrans_pb2_grpc.TransStub(channel)
